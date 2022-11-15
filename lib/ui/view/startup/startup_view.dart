a97396b66107ceb96b9796b60fae8fdbef9dd52b
Bagas Dany Aradhana <bagasdany74@gmail.com>
2022-11-16 00:05:45 +0700

    Add : Startup view / Splash Screen

---
+++ lib/ui/view/startup/startup_view.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../../../application/app/contants/constants.dart';
+import '../home/home_view.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 10),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: Constants.spacing4),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                child: AspectRatio(
+                    aspectRatio: 7 / 1,
+                    child: Image.asset('assets/logo/edspert.png')),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: Constants.spacing2),
+                child: const Text(
+                  "Jadi expert bersama edspert.id",
+                  style: TextStyle(
+                      fontSize: Constants.fontSizeLg, color: Constants.gray),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
