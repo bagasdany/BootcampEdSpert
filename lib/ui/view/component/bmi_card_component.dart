9299b596b61bb2c0126094a615a3986985a97759
Bagas Dany Aradhana <bagasdany74@gmail.com>
2022-11-16 00:02:49 +0700

    Add : Component BMI card component

---
+++ lib/ui/view/component/bmi_card_component.dart
@@ -0,0 +1,28 @@
+import 'package:edspert_bootcamp/application/app/contants/constants.dart';
+import 'package:flutter/material.dart';
+
+class BmiCardComponent extends StatelessWidget {
+  const BmiCardComponent({
+    Key? key,
+    this.child,
+    this.borderColor = Colors.white10,
+  }) : super(key: key);
+
+  final Widget? child;
+  final Color? borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(Constants.spacing4),
+        border: Border.all(
+          color: borderColor!,
+        ),
+      ),
+      margin: const EdgeInsets.all(Constants.spacing4),
+      child: child,
+    );
+  }
+}
