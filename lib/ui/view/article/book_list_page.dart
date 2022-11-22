aa0973a9e83ff5309b2afd8b4dcac9d6c4578708
Bagas Dany Aradhana <bagasdany74@gmail.com>
2022-11-22 20:51:49 +0700

    Add : [bagas] Add Refresh indicator to List Catalog

--- lib/ui/view/article/book_list_page.dart
+++ lib/ui/view/article/book_list_page.dart
@@ -44,125 +44,135 @@ class _BookListPageState extends State<BookListPage> {
           style: TextStyle(color: Constants.black),
         ),
       ),
-      body: Consumer<BookControllers>(
-        child: ShimmerListPage(
-          bookList: bookControllers!.bookList,
-        ),
-        builder: (context, controllers, child) => Container(
-          child: bookControllers!.bookList == null
-              ? child
-              : ListView.builder(
-                  itemCount: bookControllers!.bookList!.books!.length,
-                  itemBuilder: (context, index) {
-                    final currentBook =
-                        bookControllers!.bookList!.books![index];
-                    return InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => DetailBookPage(
-                              isbn: currentBook.isbn13!,
+      body: RefreshIndicator(
+        onRefresh: () {
+          bookControllers =
+              Provider.of<BookControllers>(context, listen: false);
+          return bookControllers!.fetchBookApi(context);
+        },
+        child: Consumer<BookControllers>(
+          child: ShimmerListPage(
+            bookList: bookControllers!.bookList,
+          ),
+          builder: (context, controllers, child) => Container(
+            child: bookControllers!.bookList == null
+                ? child
+                : ListView.builder(
+                    itemCount: bookControllers!.bookList!.books!.length,
+                    itemBuilder: (context, index) {
+                      final currentBook =
+                          bookControllers!.bookList!.books![index];
+                      return InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => DetailBookPage(
+                                isbn: currentBook.isbn13!,
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        color: Constants.white,
-                        margin: const EdgeInsets.only(top: Constants.spacing2),
-                        padding: const EdgeInsets.all(Constants.spacing4),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              height: 100,
-                              width: 100,
-                              child: SizedBox(
-                                child: Image.network(
-                                  currentBook.image ?? "",
-                                  fit: BoxFit.contain,
-                                  loadingBuilder: (BuildContext context,
-                                      Widget child,
-                                      ImageChunkEvent? loadingProgress) {
-                                    if (loadingProgress == null) {
+                          );
+                        },
+                        child: Container(
+                          color: Constants.white,
+                          margin:
+                              const EdgeInsets.only(top: Constants.spacing2),
+                          padding: const EdgeInsets.all(Constants.spacing4),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                height: 100,
+                                width: 100,
+                                child: SizedBox(
+                                  child: Image.network(
+                                    currentBook.image ?? "",
+                                    fit: BoxFit.contain,
+                                    loadingBuilder: (BuildContext context,
+                                        Widget child,
+                                        ImageChunkEvent? loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return SizedBox(
+                                            height: 100,
+                                            width: 100,
+                                            child: child);
+                                      }
                                       return SizedBox(
-                                          height: 100,
-                                          width: 100,
-                                          child: child);
-                                    }
-                                    return SizedBox(
-                                      height: 100,
-                                      width: 100,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            color: Constants.gray.shade200,
-                                            borderRadius:
-                                                const BorderRadius.all(
-                                                    Radius.circular(
-                                                        Constants.spacing3))),
-                                      ),
-                                    );
-                                  },
-                                  errorBuilder: (BuildContext context,
-                                      Object exception,
-                                      StackTrace? stackTrace) {
-                                    return SizedBox(
-                                      height: 100,
-                                      width: 100,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            color: Constants.gray.shade200,
-                                            borderRadius:
-                                                const BorderRadius.all(
-                                                    Radius.circular(
-                                                        Constants.spacing3))),
-                                      ),
-                                    );
-                                  },
+                                        height: 100,
+                                        width: 100,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              color: Constants.gray.shade200,
+                                              borderRadius:
+                                                  const BorderRadius.all(
+                                                      Radius.circular(
+                                                          Constants.spacing3))),
+                                        ),
+                                      );
+                                    },
+                                    errorBuilder: (BuildContext context,
+                                        Object exception,
+                                        StackTrace? stackTrace) {
+                                      return SizedBox(
+                                        height: 100,
+                                        width: 100,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                              color: Constants.gray.shade200,
+                                              borderRadius:
+                                                  const BorderRadius.all(
+                                                      Radius.circular(
+                                                          Constants.spacing3))),
+                                        ),
+                                      );
+                                    },
+                                  ),
                                 ),
                               ),
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  currentBook.title == null
-                                      ? Container()
-                                      : Text(
-                                          currentBook.title ?? "",
-                                          style: const TextStyle(),
-                                        ),
-                                  currentBook.subtitle == null ||
-                                          currentBook.subtitle == ""
-                                      ? Container()
-                                      : Container(
-                                          margin: const EdgeInsets.only(
-                                              top: Constants.spacing1),
-                                          child: Text(
-                                            currentBook.subtitle ?? "",
-                                            style: const TextStyle(
-                                                color: Constants.gray,
-                                                fontSize: Constants.fontSizeSm),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    currentBook.title == null
+                                        ? Container()
+                                        : Text(
+                                            currentBook.title ?? "",
+                                            style: const TextStyle(),
                                           ),
-                                        ),
-                                  const SizedBox(height: Constants.spacing2),
-                                  Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Text(
-                                        currentBook.price!,
-                                        style: const TextStyle(
-                                          fontSize: Constants.fontSizeLg,
-                                          fontFamily: Constants.primaryFontBold,
-                                          color: Constants.primaryColor,
-                                        ),
-                                      )),
-                                ],
-                              ),
-                            )
-                          ],
+                                    currentBook.subtitle == null ||
+                                            currentBook.subtitle == ""
+                                        ? Container()
+                                        : Container(
+                                            margin: const EdgeInsets.only(
+                                                top: Constants.spacing1),
+                                            child: Text(
+                                              currentBook.subtitle ?? "",
+                                              style: const TextStyle(
+                                                  color: Constants.gray,
+                                                  fontSize:
+                                                      Constants.fontSizeSm),
+                                            ),
+                                          ),
+                                    const SizedBox(height: Constants.spacing2),
+                                    Align(
+                                        alignment: Alignment.topLeft,
+                                        child: Text(
+                                          currentBook.price!,
+                                          style: const TextStyle(
+                                            fontSize: Constants.fontSizeLg,
+                                            fontFamily:
+                                                Constants.primaryFontBold,
+                                            color: Constants.primaryColor,
+                                          ),
+                                        )),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
+                      );
+                    },
+                  ),
+          ),
         ),
       ),
     );
